9d454968660e39c6f82a1bbf45ddc1ea09ea1031
KRENJKUMAR PATEL <patel.krenj10@gmail.com>
2026-02-13 10:20:34 -0600

    I have pushed files from PC

---
+++ TASK1.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter a number 1: "))
+num2=int(input("Enter a number 2: "))
+
+print(f"Addition: {num1 + num2}")
+print(f"Sub: {num1 - num2}")
+print(f"Multiplication: {num1 * num2}")
+print(f"Division: {num1 / num2}")
+
